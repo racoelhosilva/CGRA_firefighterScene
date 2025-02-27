48ca5bc4f405c45dd0a08ac1315b3ca43eaa0c96
Bruno Oliveira <up202208700@up.pt>
2025-02-27 11:08:35 +0000

    Copy files from tp1 to tp2
    Co-authored-by: Rodrigo Silva <up202205188@up.pt>

---
+++ tp2/MyInterface.js
@@ -0,0 +1,34 @@
+import {CGFinterface, dat} from '../lib/CGF.js';
+
+/**
+* MyInterface
+* @constructor
+*/
+export class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        
+        // init GUI. For more information on the methods, check:
+        // https://github.com/dataarts/dat.gui/blob/master/API.md
+        this.gui = new dat.GUI();
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+
+        //Slider element in GUI
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+
+        this.gui.add(this.scene, 'displayTriangle').name('Triangle');
+        this.gui.add(this.scene, 'displayParallelogram').name('Parallelogram');
+        this.gui.add(this.scene, 'displayDiamond').name('Diamond');
+        this.gui.add(this.scene, 'displayTriangleBig').name('TriangleBig');
+        this.gui.add(this.scene, 'displayTriangleSmall').name('TriangleSmall');
+
+        return true;
+    }
+}
--- tp2/README.md
+++ tp2/README.md
@@ -1,1 +1,9 @@
-# [COPY FROM PREVIOUS TP AND ADJUST]
+# CG 2024/2025
+
+## Group T13G01
+
+## TP 1 Notes
+
+In general, we found the exercises in this class pretty simple.
+
+![Screenshot 1](./screenshots/cg-t13g01-tp1-1.png)
