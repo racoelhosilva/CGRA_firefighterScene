ca25a645750d4b3409ac3f8862eb79e4e14ea1cc
Bruno Oliveira <Process-ing@users.noreply.github.com>
2025-03-20 11:38:24 +0000

    TP4 Part 1
    Co-authored-by: Rodrigo Albergaria Coelho e Silva <up202205188@edu.fe.up.pt>

---
+++ tp4/MyTangram.js
@@ -0,0 +1,184 @@
+import { CGFobject, CGFappearance, CGFtexture } from "../lib/CGF.js";
+import { MyDiamond } from "./MyDiamond.js";
+import { MyTriangle } from "./MyTriangle.js";
+import { MyParallelogram } from "./MyParallelogram.js";
+import { MyTriangleBig } from "./MyTriangleBig.js";
+import { MyTriangleSmall } from "./MyTriangleSmall.js";
+
+export class MyTangram extends CGFobject {
+  constructor(scene) {
+      super(scene);
+      this.diamond = new MyDiamond(this.scene);
+      this.triangle = new MyTriangle(this.scene);
+      this.parallelogram = new MyParallelogram(this.scene);
+      this.triangleSmall1 = new MyTriangleSmall(this.scene, false);
+      this.triangleSmall2 = new MyTriangleSmall(this.scene, true);
+      this.triangleBig1 = new MyTriangleBig(this.scene, false);
+      this.triangleBig2 = new MyTriangleBig(this.scene, true);
+
+      this.material = new CGFappearance(this.scene);
+      this.texture = new CGFtexture(this.scene, 'images/tangram.png');
+      this.material.setTexture(this.texture);
+  }
+
+  display() {
+    {
+      let rotate = [
+        Math.cos(Math.PI / 4), Math.sin(Math.PI / 4), 0, 0,
+        -Math.sin(Math.PI / 4), Math.cos(Math.PI / 4), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+      ]
+      let translate = [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        Math.sqrt(2) / 2, 3 / 2 * Math.sqrt(2), 0, 1
+      ]
+
+      this.scene.pushMatrix()
+      this.scene.multMatrix(translate)
+      this.scene.multMatrix(rotate)
+
+      this.material.apply();
+      this.diamond.display();
+
+      this.scene.popMatrix();
+    }
+
+    {
+      let rotate = [
+        Math.cos(-Math.PI / 4), Math.sin(-Math.PI / 4), 0, 0,
+        -Math.sin(-Math.PI / 4), Math.cos(-Math.PI / 4), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+      ]
+      let translate = [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        Math.sqrt(2), 0, 0, 1,
+      ]
+
+      this.scene.pushMatrix();
+      this.scene.multMatrix(translate)
+      this.scene.multMatrix(rotate);
+
+      this.triangle.display();
+
+      this.scene.popMatrix();
+    }
+
+    {
+
+      let rotate = [
+        Math.cos(Math.PI / 4), Math.sin(Math.PI / 4), 0, 0,
+        -Math.sin(Math.PI / 4), Math.cos(Math.PI / 4), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+      ]
+
+      let reflect = [
+
+        1, 0, 0, 0,
+        0, Math.cos(Math.PI), Math.sin(Math.PI), 0,
+        0, -Math.sin(Math.PI), Math.cos(Math.PI), 0,
+        0, 0, 0, 1,
+      ]
+
+      this.scene.pushMatrix()
+
+      this.scene.multMatrix(reflect)
+      this.scene.multMatrix(rotate)
+
+      this.parallelogram.display();
+
+      this.scene.popMatrix()
+    }
+
+    {
+      let rotate1 = [
+        Math.cos(Math.PI / 4), Math.sin(Math.PI / 4), 0, 0,
+        -Math.sin(Math.PI / 4), Math.cos(Math.PI / 4), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+      ]
+      let translate1 = [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        Math.sqrt(2) / 2, Math.sqrt(2) / 2, 0, 1
+      ]
+
+      let rotate2 = [
+        Math.cos(3 * Math.PI / 4), Math.sin(3 * Math.PI / 4), 0, 0,
+        -Math.sin(3 * Math.PI / 4), Math.cos(3 * Math.PI / 4), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+      ]
+      let translate2 = [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        3 * Math.sqrt(2) / 2, 3 * Math.sqrt(2) / 2, 0, 1
+      ]
+
+      this.scene.pushMatrix()
+
+
+      this.scene.multMatrix(translate1)
+      this.scene.multMatrix(rotate1)
+
+      this.triangleSmall1.display();
+
+      this.scene.popMatrix()
+      this.scene.pushMatrix()
+
+      this.scene.multMatrix(translate2)
+      this.scene.multMatrix(rotate2)
+
+      this.triangleSmall2.display();
+
+      this.scene.popMatrix()
+    }
+
+    {
+      let rotate1 = [
+        Math.cos(-Math.PI / 2), Math.sin(-Math.PI / 2), 0, 0,
+        -Math.sin(-Math.PI / 2), Math.cos(-Math.PI / 2), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+      ]
+      let rotate2 = [
+        Math.cos(Math.PI / 2), Math.sin(Math.PI / 2), 0, 0,
+        -Math.sin(Math.PI / 2), Math.cos(Math.PI / 2), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+      ]
+      let translate = [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        Math.sqrt(2) - 2, -2 * Math.sqrt(2), 0, 1,
+      ]
+
+      this.scene.pushMatrix();
+      this.scene.multMatrix(translate)
+
+      this.scene.pushMatrix();
+      this.scene.multMatrix(rotate1);
+
+      this.triangleBig1.display();
+
+      this.scene.popMatrix();
+
+      this.scene.pushMatrix();
+      this.scene.multMatrix(rotate2);
+
+      this.triangleBig2.display();
+
+      this.scene.popMatrix();
+      this.scene.popMatrix();
+    }
+  }
+}
---
+++ tp4/MyTriangleSmall.js
@@ -0,0 +1,53 @@
+import {CGFobject} from '../lib/CGF.js';
+/**
+ * MyTriangleSmall
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyTriangleSmall extends CGFobject {
+	constructor(scene, secondTriangle) {
+		super(scene);
+		this.secondTriangle = secondTriangle;
+		this.initBuffers();
+	}
+
+	initBuffers() {
+		this.vertices = [
+			-1, 0, 0,	//0
+			0, 1, 0,	//1
+			1, 0, 0		//2
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			1, 0, 2
+		];
+
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+		]
+
+		if (!this.secondTriangle) {
+			this.texCoords = [
+				0.25, 0.75,
+				0.75, 0.75,
+				0.5, 0.5
+			]
+		} else {
+			this.texCoords = [
+				0.0, 0.0,
+				0.0, 0.5,
+				0.25, 0.25
+			]
+		}
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
+
