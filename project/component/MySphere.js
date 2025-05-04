81d568d61b54dcc56c1ac2cc4ff1e69745fae3ea
Bruno Oliveira <Process-ing@users.noreply.github.com>
2025-05-04 23:28:40 +0100

    Refactor sphere

--- project/component/MySphere.js
+++ project/component/MySphere.js
@@ -11,81 +11,93 @@ export class MySphere extends CGFobject {
     this.initBuffers()
   }
 
-  getVertexCoords(stack, slice) {
+  pushVertex(x, y, z, u, v) {
+    this.vertices.push(x, y, z);
+    if (!this.inverted) {
+      this.normals.push(x, y, z);
+    } else {
+      this.normals.push(-x, -y, -z);
+    }
+    this.texCoords.push(u, v);
+  }
+
+  pushPole(isNorth, slice) {
+    const [x, y, z] = [0, isNorth ? 1 : -1, 0];
+    const [u, v] = [(2 * slice + 1) / (2 * this.slices), isNorth ? 0 : 1];
+
+    this.pushVertex(x, y, z, u, v);
+  }
+
+  pushMiddleVertex(stack, slice) {
     const gamma = 2 * Math.PI * slice / this.slices;
     const beta = Math.PI / 2 * stack / this.stacks;
-    const r = Math.cos(beta)
+    const r = Math.cos(beta);
 
-    return [r * Math.cos(gamma), Math.sin(beta), -r * Math.sin(gamma)]
+    const [x, y, z] = [r * Math.cos(gamma), Math.sin(beta), -r * Math.sin(gamma)];
+    const [u, v] = [slice / this.slices, (this.stacks - stack) / (2 * this.stacks)];
+
+    this.pushVertex(x, y, z, u, v);
   }
 
-  getTexCoords(stack, slice) {
-    return [slice / this.slices, (this.stacks - stack) / (2 * this.stacks)]
-  }
+  pushAllVertices() {
+    for (let slice = 0; slice < this.slices; slice++)
+      this.pushPole(false, slice);
 
-  pushEquatorVertex(slice) {
-    const [x, _, z] = this.getVertexCoords(0, slice);
-    const texCoords = this.getTexCoords(0, slice);
-
-    this.vertices.push(x, 0, z);
-    if (!this.inverted) {
-      this.normals.push(x, 0, z);
-    } else {
-      this.normals.push(-x, 0, -z);
-    }
-    this.texCoords.push(...texCoords)
-  }
-
-  pushVertexNormalAndMirror(stack, slice) {
-    const [x, y, z] = this.getVertexCoords(stack, slice);
-    const [tx, ty] = this.getTexCoords(stack, slice);
-
-    this.vertices.push(
-      x, y, z,
-      x, -y, z
-    );
-
-    if (!this.inverted) {
-      this.normals.push(
-        x, y, z,
-        x, -y, z,
-      );
-    } else {
-      this.normals.push(
-        -x, -y, -z,
-        -x, y, -z,
-      );
+    for (let stack = -(this.stacks - 1); stack < this.stacks; stack++) {
+      for (let slice = 0; slice <= this.slices; slice++) {
+        this.pushMiddleVertex(stack, slice);
+      }
     }
 
-    this.texCoords.push(
-      tx, ty,
-      tx, 1 - ty
-    );
+    for (let slice = 0; slice < this.slices; slice++)
+      this.pushPole(true, slice);
   }
 
-  pushPoles(slice) {
-    this.vertices.push(
-      0, 1, 0,
-      0, -1, 0,
-    );
-
-    if (!this.inverted) {
-      this.normals.push(
-        0, 1, 0,
-        0, -1, 0
-      );
+  pushTriangle(i, j, k) {
+    if (this.inverted) {
+      this.indices.push(i, j, k);
     } else {
-      this.normals.push(
-        0, -1, 0,
-        0, 1, 0
-      );
+      this.indices.push(i, k, j);
+    }
+  }
+
+  pushPoleFace(isNorth, slice) {
+    let pole, right, left;
+    if (isNorth) {
+      pole = this.slices + (2 * this.stacks - 1) * (this.slices + 1) + slice;
+      right = pole - this.slices - 1;
+      left = pole - this.slices;
+      this.pushTriangle(pole, left, right);
+
+    } else {
+      pole = slice;
+      right = pole + this.slices;
+      left = pole + this.slices + 1;
+      this.pushTriangle(pole, right, left);
+    }
+  }
+
+  pushMiddleFace(stack, slice) {
+    const bottomRight = this.slices + (this.stacks - 1 + stack) * (this.slices + 1) + slice;
+    const bottomLeft = bottomRight + 1;
+    const topRight = bottomRight + this.slices + 1;
+    const topLeft = bottomRight + this.slices + 2;
+
+    this.pushTriangle(bottomRight, topRight, topLeft);
+    this.pushTriangle(topLeft, bottomLeft, bottomRight);
+  }
+
+  pushAllFaces() {
+    for (let slice = 0; slice < this.slices; slice++)
+      this.pushPoleFace(false, slice);
+
+    for (let stack = -(this.stacks - 1); stack < this.stacks - 1; stack++) {
+      for (let slice = 0; slice < this.slices; slice++)
+        this.pushMiddleFace(stack, slice);
     }
 
-    let middleU = (2 * slice + 1) / (2 * this.slices)
-    this.texCoords.push(
-      middleU, 0,
-      middleU, 1
-    )
+    for (let slice = 0; slice < this.slices; slice++)
+      this.pushPoleFace(true, slice);
   }
 
   initBuffers() {
@@ -94,61 +106,8 @@ export class MySphere extends CGFobject {
     this.texCoords = [];
     this.indices = [];
 
-    // Equator
-    for (let slice = 0; slice <= this.slices; slice++) {
-      this.pushEquatorVertex(slice);
-    }
-
-    // Equator stacks
-    if (this.stacks > 1) {
-      this.pushVertexNormalAndMirror(1, 0);
-
-      for (let slice = 1; slice <= this.slices; slice++) {
-        this.pushVertexNormalAndMirror(1, slice);
-
-        this.indices.push(
-          slice - 1, slice, 2 * slice + this.slices + 1,
-          2 * slice + this.slices + 1, 2 * slice + this.slices - 1, slice - 1,
-          slice - 1, 2 * slice + this.slices, 2 * slice + this.slices + 2,
-          2 * slice + this.slices + 2, slice, slice - 1,
-        )
-      }
-    }
-
-    // Hemisphere middle stacks
-    for (let stack = 2; stack < this.stacks; stack++) {
-      this.pushVertexNormalAndMirror(stack, 0);
-
-      for (let slice = 1; slice <= this.slices; slice++) {
-        this.pushVertexNormalAndMirror(stack, slice);
-
-        let upperPlaneTr = (2 * stack - 1) * (this.slices + 1) + slice * 2;
-        let lowerPlaneBr = upperPlaneTr + 1;
-
-        this.indices.push(
-          upperPlaneTr - 2 * this.slices - 4, upperPlaneTr - 2 * this.slices - 2, upperPlaneTr,
-          upperPlaneTr, upperPlaneTr - 2, upperPlaneTr - 2 * this.slices - 4,
-          lowerPlaneBr - 2 * this.slices - 4, lowerPlaneBr - 2, lowerPlaneBr,
-          lowerPlaneBr, lowerPlaneBr - 2 * this.slices - 2, lowerPlaneBr - 2 * this.slices - 4,
-        );
-      }
-    }
-
-    // Poles
-    for (let slice = 0; slice < this.slices; slice++) {
-      this.pushPoles(slice);
-
-      let northPole = (2 * this.stacks - 1) * (this.slices + 1) + slice * 2;
-      let southPole = northPole + 1;
-
-      this.indices.push(
-        northPole - 2 * this.slices - 2, northPole - 2 * this.slices, northPole,
-        southPole - 2 * this.slices, southPole - 2 * this.slices - 2, southPole,
-      )
-    }
-
-    if (this.inverted)
-      this.indices.reverse()
+    this.pushAllVertices();
+    this.pushAllFaces();
 
     this.primitiveType = this.scene.gl.TRIANGLES;
     this.initGLBuffers();
