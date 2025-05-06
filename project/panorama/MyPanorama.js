66ef2b874670325f2a784e79ce8c14d9fb328bb5
Bruno Oliveira <Process-ing@users.noreply.github.com>
2025-05-06 11:02:57 +0100

    Implement ellipsoid

--- project/panorama/MyPanorama.js
+++ project/panorama/MyPanorama.js
@@ -2,10 +2,12 @@ import { CGFappearance, CGFobject } from "../../lib/CGF.js";
 import { MySphere } from "../component/MySphere.js";
 
 export class MyPanorama extends CGFobject {
+  RADIUS = 1000;
+
   constructor(scene, stacks, slices, texture) {
     super(scene);
 
-    this.sphere = new MySphere(scene, stacks, slices, true);
+    this.sphere = new MySphere(scene, this.RADIUS, stacks, slices, true);
     this.material = new CGFappearance(scene);
     this.material.setEmission(1.0, 1.0, 1.0, 1.0);
     this.material.setTexture(texture);
@@ -14,7 +16,6 @@ export class MyPanorama extends CGFobject {
   display() {
     this.scene.pushMatrix();
     this.scene.translate(...this.scene.camera.position);
-    this.scene.scale(1000, 1000, 1000);
 
     this.material.apply();
     this.sphere.display();
