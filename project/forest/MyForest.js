9de06b5b36c1ae510df9ef295cb12c2402a5ff38
Rodrigo Coelho <racoelhosilva@users.noreply.github.com>
2025-05-31 23:40:36 +0100

    feat: format forest and tree

--- project/forest/MyForest.js
+++ project/forest/MyForest.js
@@ -2,102 +2,102 @@ import { CGFobject } from "../../lib/CGF.js";
 import { MyTree } from "./MyTree.js";
 
 export class MyForest extends CGFobject {
-  MIN_RADIUS
-  MAX_TILT = Math.PI / 18;
-  MIN_RADIUS = 3;
-  MAX_RADIUS = 5;
-  MIN_HEIGHT = 35;
-  MAX_HEIGHT = 50;
+    MIN_RADIUS
+    MAX_TILT = Math.PI / 18;
+    MIN_RADIUS = 3;
+    MAX_RADIUS = 5;
+    MIN_HEIGHT = 35;
+    MAX_HEIGHT = 50;
 
-  GRID_SQUARE_SIZE = 20;
-  POS_MAX_OFFSET = 6;
+    GRID_SQUARE_SIZE = 20;
+    POS_MAX_OFFSET = 6;
 
-  constructor(scene, width, height, rows, columns, truncMaterial, crownMaterial, minColor, maxColor) {
-    super(scene);
+    constructor(scene, width, height, rows, columns, truncMaterial, crownMaterial, minColor, maxColor) {
+        super(scene);
 
-    this.rows = rows;
-    this.columns = columns;
-    this.width = width;
-    this.height = height;
+        this.rows = rows;
+        this.columns = columns;
+        this.width = width;
+        this.height = height;
 
-    this.minColor = minColor;
-    this.maxColor = maxColor;
+        this.minColor = minColor;
+        this.maxColor = maxColor;
 
-    this.trees = this.buildTrees(rows, columns, truncMaterial, crownMaterial);
-    this.displacements = this.buildDisplacements(rows, columns);
-  }
-
-  buildTrees(rows, columns, truncMaterial, crownMaterial) {
-    let trees = [];
-
-    for (let row = 0; row < rows; row++) {
-      let treeRow = [];
-      for (let col = 0; col < columns; col++)
-        treeRow.push(this.buildRandomTree(truncMaterial, crownMaterial));
-      trees.push(treeRow);
+        this.trees = this.buildTrees(rows, columns, truncMaterial, crownMaterial);
+        this.displacements = this.buildDisplacements(rows, columns);
     }
 
-    return trees;
-  }
+    buildTrees(rows, columns, truncMaterial, crownMaterial) {
+        let trees = [];
 
-  buildRandomTree(truncMaterial, crownMaterial) {
-    const tilt = this.randomBetween(-this.MAX_TILT, this.MAX_TILT);
-    const tiltAxis = Math.random() < 0.5 ? "x" : "z";
-    const radius = this.randomBetween(this.MIN_RADIUS, this.MAX_RADIUS);
-    const height = this.randomBetween(this.MIN_HEIGHT, this.MAX_HEIGHT);
-    const color = this.randomColor();
+        for (let row = 0; row < rows; row++) {
+            let treeRow = [];
+            for (let col = 0; col < columns; col++)
+                treeRow.push(this.buildRandomTree(truncMaterial, crownMaterial));
+            trees.push(treeRow);
+        }
 
-    return new MyTree(this.scene, tilt, tiltAxis, radius, height, color, truncMaterial, crownMaterial);
-  }
-
-  randomBetween(min, max) {
-    return min + Math.random() * (max - min);
-  }
-
-  randomColor() {
-    const randomFactor = Math.random();
-
-    return [
-      this.minColor[0] + randomFactor * (this.maxColor[0] - this.minColor[0]),
-      this.minColor[1] + randomFactor * (this.maxColor[1] - this.minColor[1]),
-      this.minColor[2] + randomFactor * (this.maxColor[2] - this.minColor[2])
-    ];
-  }
-
-  buildDisplacements(rows, columns) {
-    let displacements =  [];
-
-    for (let row = 0; row < rows; row++) {
-      let dispRow = [];
-      for (let col = 0; col < columns; col++)
-        dispRow.push(this.randomPosition(row, col));
-      displacements.push(dispRow);
+        return trees;
     }
 
-    return displacements;
-  }
+    buildRandomTree(truncMaterial, crownMaterial) {
+        const tilt = this.randomBetween(-this.MAX_TILT, this.MAX_TILT);
+        const tiltAxis = Math.random() < 0.5 ? "x" : "z";
+        const radius = this.randomBetween(this.MIN_RADIUS, this.MAX_RADIUS);
+        const height = this.randomBetween(this.MIN_HEIGHT, this.MAX_HEIGHT);
+        const color = this.randomColor();
 
-  randomPosition(row, col) {
-    const dx = this.randomBetween(-this.POS_MAX_OFFSET, this.POS_MAX_OFFSET);
-    const dz = this.randomBetween(-this.POS_MAX_OFFSET, this.POS_MAX_OFFSET);
-
-    return [
-      (col + 0.5 - this.columns / 2) * this.width / this.columns + dx,
-      (row + 0.5 - this.rows / 2) * this.height / this.rows + dz
-    ];
-  }
-
-  display() {
-    for (let row = 0; row < this.rows; row++) {
-      for (let col = 0; col < this.columns; col++) {
-        let tree = this.trees[row][col];
-        let [dx, dz] = this.displacements[row][col];
-
-        this.scene.pushMatrix();
-        this.scene.translate(dx, 0, dz);
-        tree.display();
-        this.scene.popMatrix();
-      }
+        return new MyTree(this.scene, tilt, tiltAxis, radius, height, color, truncMaterial, crownMaterial);
     }
-  }
+
+    randomBetween(min, max) {
+        return min + Math.random() * (max - min);
+    }
+
+    randomColor() {
+        const randomFactor = Math.random();
+
+        return [
+            this.minColor[0] + randomFactor * (this.maxColor[0] - this.minColor[0]),
+            this.minColor[1] + randomFactor * (this.maxColor[1] - this.minColor[1]),
+            this.minColor[2] + randomFactor * (this.maxColor[2] - this.minColor[2])
+        ];
+    }
+
+    buildDisplacements(rows, columns) {
+        let displacements = [];
+
+        for (let row = 0; row < rows; row++) {
+            let dispRow = [];
+            for (let col = 0; col < columns; col++)
+                dispRow.push(this.randomPosition(row, col));
+            displacements.push(dispRow);
+        }
+
+        return displacements;
+    }
+
+    randomPosition(row, col) {
+        const dx = this.randomBetween(-this.POS_MAX_OFFSET, this.POS_MAX_OFFSET);
+        const dz = this.randomBetween(-this.POS_MAX_OFFSET, this.POS_MAX_OFFSET);
+
+        return [
+            (col + 0.5 - this.columns / 2) * this.width / this.columns + dx,
+            (row + 0.5 - this.rows / 2) * this.height / this.rows + dz
+        ];
+    }
+
+    display() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.columns; col++) {
+                let tree = this.trees[row][col];
+                let [dx, dz] = this.displacements[row][col];
+
+                this.scene.pushMatrix();
+                this.scene.translate(dx, 0, dz);
+                tree.display();
+                this.scene.popMatrix();
+            }
+        }
+    }
 }
--- project/forest/MyTree.js
+++ project/forest/MyTree.js
@@ -4,96 +4,96 @@ import { MyPyramid } from "../component/MyPyramid.js";
 import { MyRegularPolygon } from "../component/MyRegularPolygon.js";
 
 export class MyTree extends CGFobject {
-  crownSides = 6;
-  rootHeight = 8;
+    crownSides = 6;
+    rootHeight = 8;
 
-  constructor(scene, tilt, tiltAxis, trunkRadius, height, crownColor, trunkTexture, crownTexture) {
-    super(scene);
+    constructor(scene, tilt, tiltAxis, trunkRadius, height, crownColor, trunkTexture, crownTexture) {
+        super(scene);
 
-    this.tilt = tilt;
-    this.tiltAxis = tiltAxis == "x" ? [1, 0, 0] : [0, 0, 1];
+        this.tilt = tilt;
+        this.tiltAxis = tiltAxis == "x" ? [1, 0, 0] : [0, 0, 1];
 
-    this.trunc = this.buildTrunc(trunkRadius, height);
-    this.truncMaterial = this.buildTruncMaterial();
-    this.truncMaterial.setTexture(trunkTexture);
+        this.trunc = this.buildTrunc(trunkRadius, height);
+        this.truncMaterial = this.buildTruncMaterial();
+        this.truncMaterial.setTexture(trunkTexture);
 
-    [this.crown, this.crownStart, this.crownStep] = this.buildCrown(trunkRadius, height);
-    this.crownBase = new MyRegularPolygon(this.scene, 6, 1);
-    this.crownMaterial = this.buildCrownMaterial(crownColor);
-    this.crownMaterial.setTexture(crownTexture);
-  }
-
-  buildTrunc(trunkRadius, height) {
-    const realRadius = (height + this.rootHeight) * trunkRadius / height;
-    return new MyCone(this.scene, 8, realRadius, height + this.rootHeight);
-  }
-
-  buildTruncMaterial() {
-    let material = new CGFappearance(this.scene);
-    material.setAmbient(0.306,0.208,0.125, 1.0);
-    material.setDiffuse(0.306,0.208,0.125, 1.0);
-    material.setSpecular(0.306,0.208,0.125, 1.0);
-    material.setShininess(10.0);
-
-    return material;
-  }
-
-  buildCrown(trunkRadius, height) {
-    let numPyramids = Math.round(height * 0.8 / trunkRadius) - 1;
-    let pyramidHeight = height * 1.6 / (numPyramids + 1);
-
-    let crown = [];
-    for (let pyramid = 0; pyramid < numPyramids; pyramid++) {
-      const pyramidRadius = trunkRadius * (3 - 2 * pyramid / (numPyramids + 1));
-
-      crown.push(new MyPyramid(this.scene, this.crownSides, pyramidRadius, pyramidHeight));
+        [this.crown, this.crownStart, this.crownStep] = this.buildCrown(trunkRadius, height);
+        this.crownBase = new MyRegularPolygon(this.scene, 6, 1);
+        this.crownMaterial = this.buildCrownMaterial(crownColor);
+        this.crownMaterial.setTexture(crownTexture);
     }
 
-    return [crown, height * 0.2, pyramidHeight / 2];
-  }
+    buildTrunc(trunkRadius, height) {
+        const realRadius = (height + this.rootHeight) * trunkRadius / height;
+        return new MyCone(this.scene, 8, realRadius, height + this.rootHeight);
+    }
 
-  buildCrownBase() {
-    return new MyRegularPolygon(this.scene, this.crownSides);
-  }
+    buildTruncMaterial() {
+        let material = new CGFappearance(this.scene);
+        material.setAmbient(0.306, 0.208, 0.125, 1.0);
+        material.setDiffuse(0.306, 0.208, 0.125, 1.0);
+        material.setSpecular(0.306, 0.208, 0.125, 1.0);
+        material.setShininess(10.0);
 
-  buildCrownMaterial(crownColor) {
-    const [r, g, b] = crownColor;
+        return material;
+    }
 
-    let material = new CGFappearance(this.scene);
-    material.setAmbient(r * 0.75, g * 0.75, b * 0.75, 1.0);
-    material.setDiffuse(r, g, b, 1.0);
-    material.setSpecular(r * 0.25, g * 0.25, b * 0.25, 1.0);
+    buildCrown(trunkRadius, height) {
+        let numPyramids = Math.round(height * 0.8 / trunkRadius) - 1;
+        let pyramidHeight = height * 1.6 / (numPyramids + 1);
 
-    return material;
-  }
+        let crown = [];
+        for (let pyramid = 0; pyramid < numPyramids; pyramid++) {
+            const pyramidRadius = trunkRadius * (3 - 2 * pyramid / (numPyramids + 1));
 
-  display() {
-    this.scene.pushMatrix();
+            crown.push(new MyPyramid(this.scene, this.crownSides, pyramidRadius, pyramidHeight));
+        }
 
-    this.scene.rotate(this.tilt, ...this.tiltAxis);
+        return [crown, height * 0.2, pyramidHeight / 2];
+    }
 
-    this.scene.pushMatrix();
-    this.scene.translate(0, -this.rootHeight, 0);
-    this.truncMaterial.apply();
-    this.trunc.display();
-    this.scene.popMatrix();
+    buildCrownBase() {
+        return new MyRegularPolygon(this.scene, this.crownSides);
+    }
 
-    this.scene.translate(0, this.crownStart, 0);
-    this.crownMaterial.apply();
-    this.crown.forEach(crownPart => {
-      crownPart.display();
-      this.displayCrownBase(crownPart.radius);
-      this.scene.translate(0, this.crownStep, 0);
-    });
+    buildCrownMaterial(crownColor) {
+        const [r, g, b] = crownColor;
 
-    this.scene.popMatrix();
-  }
+        let material = new CGFappearance(this.scene);
+        material.setAmbient(r * 0.75, g * 0.75, b * 0.75, 1.0);
+        material.setDiffuse(r, g, b, 1.0);
+        material.setSpecular(r * 0.25, g * 0.25, b * 0.25, 1.0);
 
-  displayCrownBase(radius) {
-    this.scene.pushMatrix();
-    this.scene.scale(radius, 1, radius);
-    this.scene.rotate(Math.PI, 1, 0, 0);
-    this.crownBase.display();
-    this.scene.popMatrix();
-  }
+        return material;
+    }
+
+    display() {
+        this.scene.pushMatrix();
+
+        this.scene.rotate(this.tilt, ...this.tiltAxis);
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, -this.rootHeight, 0);
+        this.truncMaterial.apply();
+        this.trunc.display();
+        this.scene.popMatrix();
+
+        this.scene.translate(0, this.crownStart, 0);
+        this.crownMaterial.apply();
+        this.crown.forEach(crownPart => {
+            crownPart.display();
+            this.displayCrownBase(crownPart.radius);
+            this.scene.translate(0, this.crownStep, 0);
+        });
+
+        this.scene.popMatrix();
+    }
+
+    displayCrownBase(radius) {
+        this.scene.pushMatrix();
+        this.scene.scale(radius, 1, radius);
+        this.scene.rotate(Math.PI, 1, 0, 0);
+        this.crownBase.display();
+        this.scene.popMatrix();
+    }
 }
