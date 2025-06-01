98f26681997be606f4b601624f7b6821f6a2c555
Rodrigo Coelho <racoelhosilva@users.noreply.github.com>
2025-06-01 09:06:23 +0100

    feat: format of fire code

--- project/fire/MyBlaze.js
+++ project/fire/MyBlaze.js
@@ -2,51 +2,51 @@ import { CGFobject } from '../../lib/CGF.js';
 import { MyTriangle } from '../component/MyTriangle.js';
 
 export class MyBlaze extends CGFobject {
-  FIRE_MIN_GREEN = 0.2;
-  FIRE_MAX_GREEN = 0.7;
+    FIRE_MIN_GREEN = 0.2;
+    FIRE_MAX_GREEN = 0.7;
 
-  constructor(scene, minHeight, maxHeight, radiusRatio, material, fire) {
-    super(scene);
+    constructor(scene, minHeight, maxHeight, radiusRatio, material, fire) {
+        super(scene);
 
-    this.triangle = this.buildTriangle(minHeight, maxHeight, radiusRatio);
+        this.triangle = this.buildTriangle(minHeight, maxHeight, radiusRatio);
 
-    this.material = material;
-    this.fire = fire;
+        this.material = material;
+        this.fire = fire;
 
-    this.green = this.buildGreen(this.triangle.p3, minHeight, maxHeight);
-    this.randomFactor = 0.5 + Math.random() * 1.5;
-  }
+        this.green = this.buildGreen(this.triangle.p3, minHeight, maxHeight);
+        this.randomFactor = 0.5 + Math.random() * 1.5;
+    }
 
-  buildTriangle(minHeight, maxHeight, radiusRatio) {
-    const height = minHeight + Math.random() ** 1.4 * (maxHeight - minHeight);
-    const radius = radiusRatio * height;
+    buildTriangle(minHeight, maxHeight, radiusRatio) {
+        const height = minHeight + Math.random() ** 1.4 * (maxHeight - minHeight);
+        const radius = radiusRatio * height;
 
-    const base1 = this.buildRandomBaseVertex(radius);
-    const base2 = this.buildRandomBaseVertex(radius);
-    const top = this.buildRandomTopVertex(height, base1, base2);
+        const base1 = this.buildRandomBaseVertex(radius);
+        const base2 = this.buildRandomBaseVertex(radius);
+        const top = this.buildRandomTopVertex(height, base1, base2);
 
-    return new MyTriangle(this.scene, base1, base2, top, Math.round(8 * top[1] / maxHeight), true);
-  }
+        return new MyTriangle(this.scene, base1, base2, top, Math.round(8 * top[1] / maxHeight), true);
+    }
 
-  buildGreen(topVertex, minHeight, maxHeight) {
-    return this.FIRE_MIN_GREEN + ((maxHeight - topVertex[1]) / (maxHeight - minHeight)) * (this.FIRE_MAX_GREEN - this.FIRE_MIN_GREEN);
-  }
+    buildGreen(topVertex, minHeight, maxHeight) {
+        return this.FIRE_MIN_GREEN + ((maxHeight - topVertex[1]) / (maxHeight - minHeight)) * (this.FIRE_MAX_GREEN - this.FIRE_MIN_GREEN);
+    }
 
-  buildRandomBaseVertex(radius) {
-    const [r, theta] = [Math.random() * radius, Math.random() * 2 * Math.PI];
-    const [x, z] = [r * Math.cos(theta), r * Math.sin(theta)];
+    buildRandomBaseVertex(radius) {
+        const [r, theta] = [Math.random() * radius, Math.random() * 2 * Math.PI];
+        const [x, z] = [r * Math.cos(theta), r * Math.sin(theta)];
 
-    return [x, 0, z];
-  }
+        return [x, 0, z];
+    }
 
-  buildRandomTopVertex(height, baseVertex1, baseVertex2) {
-    const [x, z] = [(baseVertex1[0] + baseVertex2[0]) / 2, (baseVertex1[2] + baseVertex2[2]) / 2];
-    return [x, height, z];
-  }
+    buildRandomTopVertex(height, baseVertex1, baseVertex2) {
+        const [x, z] = [(baseVertex1[0] + baseVertex2[0]) / 2, (baseVertex1[2] + baseVertex2[2]) / 2];
+        return [x, height, z];
+    }
 
-  display() {
-    this.fire.shader.setUniformsValues({ green: this.green, randomFactor: this.randomFactor });
-    this.material.apply();
-    this.triangle.display();
-  }
+    display() {
+        this.fire.shader.setUniformsValues({ green: this.green, randomFactor: this.randomFactor });
+        this.material.apply();
+        this.triangle.display();
+    }
 }
--- project/fire/MyFire.js
+++ project/fire/MyFire.js
@@ -2,85 +2,85 @@ import { CGFappearance, CGFobject } from '../../lib/CGF.js';
 import { MyBlaze } from './MyBlaze.js';
 
 export class MyFire extends CGFobject {
-  constructor(scene, radius, height, numBlazes, center, texture, shader) {
-    super(scene);
+    constructor(scene, radius, height, numBlazes, center, texture, shader) {
+        super(scene);
 
-    this.radius = radius;
-    this.height = height;
-    this.center = center;
-    this.numBlazes = numBlazes;
+        this.radius = radius;
+        this.height = height;
+        this.center = center;
+        this.numBlazes = numBlazes;
 
-    this.material = new CGFappearance(this.scene);
-    this.material.setAmbient(0.0, 0.0, 0.0, 0.0);
-    this.material.setDiffuse(0.0, 0.0, 0.0, 0.0);
-    this.material.setSpecular(0.0, 0.0, 0.0, 0.0);
-    this.material.setTexture(texture);
+        this.material = new CGFappearance(this.scene);
+        this.material.setAmbient(0.0, 0.0, 0.0, 0.0);
+        this.material.setDiffuse(0.0, 0.0, 0.0, 0.0);
+        this.material.setSpecular(0.0, 0.0, 0.0, 0.0);
+        this.material.setTexture(texture);
 
-    this.shader = shader;
+        this.shader = shader;
 
-    this.blazes = this.buildBlazes();
-    this.heightFactor = 1.0;
-  }
-
-  buildBlazes() {
-    let blazes = [];
-    for (let i = 0; i < this.numBlazes; i++) {
-      blazes.push(new MyBlaze(this.scene, this.height / 3, this.height, this.radius / this.height, this.material, this));
-    }
-    return blazes;
-  }
-
-  getHeightFactor() {
-    return this.heightFactor;
-  }
-
-  setHeightFactor(heightFactor) {
-    this.heightFactor = heightFactor;
-  }
-
-  display() {
-    if (this.heightFactor == 0)
-      return;  // If height factor is 0, no need to display
-
-    this.scene.pushMatrix();
-    this.scene.translate(this.center[0], this.center[1], this.center[2]);
-    this.scene.scale(1, this.heightFactor, 1);
-
-    this.scene.setActiveShader(this.shader);
-
-    for (let i = 0; i < this.numBlazes; i++) {
-      this.blazes[i].display();
+        this.blazes = this.buildBlazes();
+        this.heightFactor = 1.0;
     }
 
-    this.scene.setActiveShader(this.scene.defaultShader);
-    this.scene.popMatrix();
-  }
-
-  difference(v1, v2) {
-    return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]];
-  }
-
-  squaredLength(v) {
-    return v[0] * v[0] + v[2] * v[2];
-  }
-
-  collidesWith(position) {
-    return this.squaredLength(this.difference(position, this.center)) < this.radius * this.radius;
-  }
-
-  static generateFires(scene, topLeft, bottomRight, numFires, texture, shader) {
-    const fires = [];
-
-    for (let i = 0; i < numFires; i++) {
-      const radius = 10 + Math.random() * (40 - 10);
-      const height = radius * 1.5;
-      const centerX = Math.random() * (bottomRight[0] - topLeft[0]) + topLeft[0];
-      const centerZ = Math.random() * (bottomRight[2] - topLeft[2]) + topLeft[2];
-      const centerY = 0;  // Ground level
-
-      fires.push(new MyFire(scene, radius, height, 10, [centerX, centerY, centerZ], texture, shader));
+    buildBlazes() {
+        let blazes = [];
+        for (let i = 0; i < this.numBlazes; i++) {
+            blazes.push(new MyBlaze(this.scene, this.height / 3, this.height, this.radius / this.height, this.material, this));
+        }
+        return blazes;
     }
 
-    return fires;
-  }
+    getHeightFactor() {
+        return this.heightFactor;
+    }
+
+    setHeightFactor(heightFactor) {
+        this.heightFactor = heightFactor;
+    }
+
+    display() {
+        if (this.heightFactor == 0)
+            return;  // If height factor is 0, no need to display
+
+        this.scene.pushMatrix();
+        this.scene.translate(this.center[0], this.center[1], this.center[2]);
+        this.scene.scale(1, this.heightFactor, 1);
+
+        this.scene.setActiveShader(this.shader);
+
+        for (let i = 0; i < this.numBlazes; i++) {
+            this.blazes[i].display();
+        }
+
+        this.scene.setActiveShader(this.scene.defaultShader);
+        this.scene.popMatrix();
+    }
+
+    difference(v1, v2) {
+        return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]];
+    }
+
+    squaredLength(v) {
+        return v[0] * v[0] + v[2] * v[2];
+    }
+
+    collidesWith(position) {
+        return this.squaredLength(this.difference(position, this.center)) < this.radius * this.radius;
+    }
+
+    static generateFires(scene, topLeft, bottomRight, numFires, texture, shader) {
+        const fires = [];
+
+        for (let i = 0; i < numFires; i++) {
+            const radius = 10 + Math.random() * (40 - 10);
+            const height = radius * 1.5;
+            const centerX = Math.random() * (bottomRight[0] - topLeft[0]) + topLeft[0];
+            const centerZ = Math.random() * (bottomRight[2] - topLeft[2]) + topLeft[2];
+            const centerY = 0;  // Ground level
+
+            fires.push(new MyFire(scene, radius, height, 10, [centerX, centerY, centerZ], texture, shader));
+        }
+
+        return fires;
+    }
 }
